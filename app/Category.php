25f3347210bce560469cc6f41430603917446953
yalin <yalincobanoglu@yandex.com>
2018-06-25 19:24:35 +0300

    category content relation

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    
+    public function contents()
+    {
+        return $this->hasMany('App\Content', 'category_id');   
+    }
 }
