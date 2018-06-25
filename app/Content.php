00c9e27ffeae4d1a61e4dd37d720c950cc19bc72
yalin <yalincobanoglu@yandex.com>
2018-06-25 19:24:49 +0300

    content cat relation and resizable trait

--- app/Content.php
+++ app/Content.php
@@ -3,9 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Resizable;
 
 class Content extends Model
 {
-    //
+    use Resizable;
+
     protected $table = "contents";
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');        
+    }
 }
