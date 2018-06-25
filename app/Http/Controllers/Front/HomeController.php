f98e99b08d8eaad003ff63e8082ecf550a52b6b7
yalin <yalincobanoglu@yandex.com>
2018-06-25 19:42:01 +0300

    date condition for most read

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use App\Category;
 use App\Content;
+use Carbon\Carbon;
 
 
 class HomeController extends Controller 
@@ -13,8 +14,14 @@ class HomeController extends Controller
     {
         $categories = Category::orderBy('order', 'asc')->get();
 
-        $contentsByCategories = Category::with('contents')->get();
+        $contentsByCategories = Category::with(['contents' => function ($query) {
+            $query->take(2);
+        }])->get();
 
-        return view('pages.homepage', compact('categories', 'contentsByCategories'));
+        $date = Carbon::now()->subDay(1)->toDateTimeString();
+
+        $mostRead = Content::where('created_at', '>', $date)->orderBy('show', 'asc')->take(10)->get();
+
+        return view('pages.homepage', compact('categories', 'contentsByCategories', 'mostRead'));
     } 
 }
