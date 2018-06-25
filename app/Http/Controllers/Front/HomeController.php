a240c2a8bd6e3be54848afd10f0a4fbcf37f8a22
yalin <yalincobanoglu@yandex.com>
2018-06-25 18:01:46 +0300

    home cnt

---
+++ app/Http/Controllers/Front/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+
+
+class HomeController extends Controller 
+{
+    public function index()
+    {
+        return view('pages.homepage');
+    } 
+}
