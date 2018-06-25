a87bb67c5991687e03265438ad551b50ee1cd039
yalin <yalincobanoglu@yandex.com>
2018-06-25 18:01:52 +0300

    route

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'Front\HomeController@index');
 
 
 Route::group(['prefix' => 'admin'], function () {
