1f44e553486ee5f3226bb6e3c17f4b060a15ecce
yalin <yalincobanoglu@yandex.com>
2018-06-26 18:06:05 +0300

    cat detail forelse

--- resources/views/pages/category-detail.blade.php
+++ resources/views/pages/category-detail.blade.php
@@ -3,27 +3,37 @@
 @section('content')
 
 
+<div class="category-detail">
 <div class="row">
-    <div class="col-md-12">
+        <div class="col-md-12">
         <h3 class="category-detail-title">{{$category->title}}</h3>
         </div>
-            @foreach($contents as $cc)
+
+            
+            @forelse($contents as $cc)
                 <div class="col-xs-12 col-md-4 mb-3" style="">
                     <div class="card home-card" >
                         <a href="{{route('content.detail', ['id' => $cc->id, 'slug' => $cc->slug])}}">
-                    <img class="card-img-top" src="{{Voyager::image($cc->thumbnail('cropped270', 'images'))}}" alt="Card image cap">
-                    <div class="card-body">
-                        <h6 class="card-sm-title">{{$cc->title}}</h6>
-                    </div>
-                </a>
+                            <img class="card-img-top" src="{{Voyager::image($cc->thumbnail('cropped540', 'images'))}}" alt="Card image cap">
+                            <div class="card-body">
+                                <h6 class="card-sm-title">{{$cc->title}}</h6>
+                            </div>
+                        </a>
                     </div>
                 </div>
-            @endforeach
+            @empty
+                <div class="col">
+                    <p>Category empty.</p>
+                </div>
+            @endforelse
 
-        </div>
-        <div class="row">
-            <div class="col d-flex justify-content-center">
-                {{ $contents->links() }}
+            <div class="row">
+                <div class="col d-flex justify-content-center">
+                    {{ $contents->links() }}
+                </div>
             </div>
+            
         </div>
+    </div>
+    
 @endsection
