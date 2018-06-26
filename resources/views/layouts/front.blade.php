dbecb2df7c2e19874a1b9d8af0b159b801aebf86
yalin <yalincobanoglu@yandex.com>
2018-06-26 10:43:00 +0300

    header link

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -9,12 +9,12 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/style.css')}}"> 
 
-    <title>Volanews</title>
+  <title>Volanews</title>
   </head>
   <body>
 
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top d-flex justify-content-center">
-      <a class="navbar-brand" href="#">VOLANEWS</a>
+      <a class="navbar-brand" href="{{route('homepage')}}">VOLANEWS</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
