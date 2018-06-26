b2662fb8567f35a095a6ba0290dc6207326ae58b
yalin <yalincobanoglu@yandex.com>
2018-06-26 22:35:08 +0300

    updated_at

--- resources/views/pages/content-detail.blade.php
+++ resources/views/pages/content-detail.blade.php
@@ -3,12 +3,15 @@
 @section('content')
 
     <div class="row">
-            <div class="col-md-12">
+            
+            <div class="col-md-8">
                 <h2 class="category-detail-title">{{$content->title}}</h2>
-                <div class="content-date">{{$content->updated_at->format('Y-m-d H:i:s') }}</div>
-            </div>
-
-            <div class="col-md-12">
+                <div class="content-date d-flex justify-content-end">
+                    @if($content->created_at <> $content->updated_at)
+                    Updated: {{$content->updated_at->format(setting('site.datetime_format'))}} | 
+                    @endif
+                    {{$content->created_at->format(setting('site.datetime_format')) }}
+                </div>
                 <div class="row">
                     <div class="col d-flex justify-content-center">
                         @if($content->images !== "")
@@ -18,16 +21,27 @@
                 </div>
                 <div class="row">
                     <div class="col">
+                        @isset($content->summary)
+                            <div class="content-summary">
+                                {!! $content->summary !!}
+                            </div>
+                        @endif
                         <div class="content-detail">
-                            <p>{{$content->title}}</p>
                             {!! $content->body !!}
                         </div>
                     </div>
                 </div>
+                {{-- Other Contents --}}
+                @include('parts.sub-categories')
+            </div>
+
+
+            <div class="col-md-4">
+                @include('parts.most-reads')
             </div>
 
         </div>
 
-    @include('parts.home-two-columns')
+    {{-- @include('parts.home-two-columns') --}}
         
 @endsection
