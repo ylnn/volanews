3e002c158a656a2f8a33822ed17ed320f5998291
yalin <yalincobanoglu@yandex.com>
2018-06-26 09:46:04 +0300

    cat detail title

---
+++ resources/views/pages/category-detail.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.front')
+
+@section('content')
+
+
+<div class="row">
+    <div class="col-md-12">
+        <h3 class="category-detail-title">{{$category->title}}</h3>
+        </div>
+            @foreach($contents as $cc)
+                <div class="col-xs-12 col-md-4 mb-3" style="">
+                    <div class="card home-card" >
+                    <img class="card-img-top" src="{{Voyager::image($cc->thumbnail('cropped270', 'images'))}}" alt="Card image cap">
+                    <div class="card-body">
+                        <h6 class="card-sm-title">{{$cc->title}}</h6>
+                    </div>
+                    </div>
+                </div>
+            @endforeach
+
+        </div>
+        <div class="row">
+            <div class="col d-flex justify-content-center">
+                {{ $contents->links() }}
+            </div>
+        </div>
+@endsection
