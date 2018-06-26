70751efcf45c995c7e26fb1261352902320f80ce
yalin <yalincobanoglu@yandex.com>
2018-06-26 09:45:57 +0300

    front css asset

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css"> 
+    <link rel="stylesheet" href="{{asset('css/style.css')}}"> 
 
     <title>Volanews</title>
   </head>
