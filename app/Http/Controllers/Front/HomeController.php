3dd1a6247a7891d5499ddace4b61fd3e06906ebe
yalin <yalincobanoglu@yandex.com>
2018-06-26 09:15:06 +0300

    home controller index refactor

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -6,22 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Category;
 use App\Content;
 use Carbon\Carbon;
+use App\MainContent;
 
 
 class HomeController extends Controller 
 {
     public function index()
     {
-        $categories = Category::orderBy('order', 'asc')->get();
-
-        $contentsByCategories = Category::with(['contents' => function ($query) {
-            $query->take(2);
-        }])->get();
-
-        $date = Carbon::now()->subDay(1)->toDateTimeString();
-
-        $mostRead = Content::where('created_at', '>', $date)->orderBy('show', 'asc')->take(10)->get();
-
-        return view('pages.homepage', compact('categories', 'contentsByCategories', 'mostRead'));
+        return view('pages.homepage');
     } 
 }
