de3b8a7e756bafd29ca0f38ee1bbc368551c799d
yalin <yalincobanoglu@yandex.com>
2018-06-26 09:15:32 +0300

    homepage.blade content show

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.front')
 
 @section('content')
+    @if(count($mainContents) > 0)
+    @php 
+        $first = $mainContents[0]->content;
+    @endphp
     <div class="row">
         <div class="col-xs-12 col-md-12 col-lg-12 col-xl-6 mb-3">
             <div class="row">
                 <div class="col-xs-12 col-md-6 mb-3">
                         <div class="card home-card-big" >
-                        <img class="card-img-top" src="holder.js/540x434" alt="Card image cap">
+                        <img class="card-img-top" src="{{Voyager::image($first->thumbnail('cropped540', 'images'))}}" alt="Card image cap">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                            <h5 class="card-title">{{$first->title}}</h5>
                         </div>
                         </div>
                     </div>
@@ -16,41 +20,22 @@
         </div>
         <div class="col-xs-12 col-md-12 col-lg-12 col-xl-6 mb-3">
             <div class="row">
-                <div class="col-xs-12 col-md-6 mb-3" style="">
-                    <div class="card home-card" >
-                    <img class="card-img-top" src="holder.js/270x180" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-sm-title">Card title</h5>
+            @foreach($mainContents as $mc)
+                @if ($loop->first)
+                    @php continue; @endphp
+                @endif
+                
+                    <div class="col-xs-12 col-md-6 mb-3" style="">
+                        <div class="card home-card" >
+                        <img class="card-img-top" src="{{Voyager::image($mc->content->thumbnail('cropped270', 'images'))}}" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-sm-title">{{$mc->content->title}}</h5>
+                        </div>
+                        </div>
                     </div>
-                    </div>
+                    @endforeach
                 </div>
-                <div class="col-xs-12 col-md-6 mb-3">
-                    <div class="card home-card" >
-                    <img class="card-img-top" src="holder.js/270x180" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-sm-title">Card title</h5>
-                    </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-md-6 mb-3" style="">
-                    <div class="card home-card" >
-                    <img class="card-img-top" src="holder.js/270x180" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-sm-title">Card title</h5>
-                    </div>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-md-6 mb-3">
-                    <div class="card home-card" >
-                    <img class="card-img-top" src="holder.js/270x180" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-sm-title">Card title</h5>
-                    </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
+    @endif
 @endsection
