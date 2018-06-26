8766598455b1f7040da6683f296dee5afd770c93
yalin <yalincobanoglu@yandex.com>
2018-06-26 09:14:54 +0300

    view composers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Category;
+use App\Content;
+use App\MainContent;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +17,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer(['layouts.front'], function($view){
+            // categories
+            $view->with('categories', Category::orderBy('order', 'asc')->get());
+            
+            // contents by categories
+            $view->with('contentsByCategories', Category::with(['contents' => function ($query) {
+                $query->take(2);
+            }])->get());
+
+            // Most readed contents
+            $date = Carbon::now()->subDay(1)->toDateTimeString();            
+            $view->with('mostRead', Content::where('created_at', '>', $date)->orderBy('show', 'asc')->take(10)->get());
+
+        });
+
+
+        view()->composer(['pages.homepage'], function($view){
+            // main contents | headers news
+            $view->with('mainContents', MainContent::with('content')->orderBy('order', 'asc')->take(5)->get());
+        });
     }
 
     /**
