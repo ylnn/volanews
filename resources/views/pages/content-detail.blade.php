fff4c3474d4d6bd4ced434dc2300d17d47d50fea
yalin <yalincobanoglu@yandex.com>
2018-06-26 10:57:29 +0300

    cont detail

--- resources/views/pages/content-detail.blade.php
+++ resources/views/pages/content-detail.blade.php
@@ -8,7 +8,21 @@
             </div>
 
             <div class="col-md-12">
-                {{$content->body}}
+                <div class="row">
+                    <div class="col d-flex justify-content-center">
+                        @if($content->images !== "")
+                        <img class="img-responsive" src="{{asset('storage/'.$content->images)}}" alt="">
+                        @endif
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="content-detail">
+                            <p>{{$content->title}}</p>
+                            {!! $content->body !!}
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
