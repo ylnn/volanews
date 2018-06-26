f37dfaa96687183b182f78d74e634d26bf5beacc
yalin <yalincobanoglu@yandex.com>
2018-06-26 18:05:53 +0300

    front title

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -9,15 +9,20 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/style.css')}}"> 
 
-  <title>Volanews</title>
+    @isset($title)
+      <title>{{$title}} | {{setting('site.title')}}</title>
+    @else
+      <title>{{setting('site.title')}}</title>
+    @endif
+
   </head>
   <body>
 
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top d-flex justify-content-center">
       <a class="navbar-brand" href="{{route('homepage')}}">VOLANEWS</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+      {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-      </button>
+      </button> --}}
     </nav>
 
     <main role="main" class="container">
