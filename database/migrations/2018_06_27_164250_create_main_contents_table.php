54ed35011866fe0b6398152c4e3784e665e56dd9
yalin <yalincobanoglu@yandex.com>
2018-06-27 19:50:20 +0300

    main contents migration

---
+++ database/migrations/2018_06_27_164250_create_main_contents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMainContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('main_contents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('content_id');
+            $table->unsignedInteger('order');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('main_contents');
+    }
+}
